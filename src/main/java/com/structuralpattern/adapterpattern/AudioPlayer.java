1776f278d776749f6038f781325907cae2285c07
louiehuang <louiehuang21@gmail.com>
2019-05-12 20:53:54 -0700

    adapter pattern

---
+++ src/main/java/com/structuralpattern/adapterpattern/AudioPlayer.java
@@ -0,0 +1,19 @@
+package com.structuralpattern.adapterpattern;
+
+public class AudioPlayer implements MediaPlayer {
+    MediaAdapter mediaAdapter;
+
+    @Override
+    public void play(String audioType, String fileName) {
+        if (audioType.equalsIgnoreCase("mp3")) {
+            //inbuilt support to play mp3 music files
+            System.out.println("Playing mp3 file. Name: " + fileName);
+        } else if (audioType.equalsIgnoreCase("vlc") || audioType.equalsIgnoreCase("mp4")) {
+            //mediaAdapter is providing support to play other file formats
+            mediaAdapter = new MediaAdapter(audioType);
+            mediaAdapter.play(audioType, fileName);
+        } else {
+            System.out.println("Invalid media. " + audioType + " format not supported");
+        }
+    }
+}
