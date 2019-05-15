6c58628e84c1eeac36e97657a80bd6a5295fb431
louiehuang <louiehuang21@gmail.com>
2019-05-14 17:20:59 -0700

    null object pattern

---
+++ src/main/java/com/behavioralpattern/nullobjectpattern/NullCustomer.java
@@ -0,0 +1,13 @@
+package com.behavioralpattern.nullobjectpattern;
+
+public class NullCustomer extends AbstractCustomer {
+    @Override
+    public String getName() {
+        return "Not Available in Customer Database";
+    }
+
+    @Override
+    public boolean isNil() {
+        return true;
+    }
+}
---
+++ src/main/java/com/behavioralpattern/nullobjectpattern/RealCustomer.java
@@ -0,0 +1,17 @@
+package com.behavioralpattern.nullobjectpattern;
+
+public class RealCustomer extends AbstractCustomer {
+    public RealCustomer(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean isNil() {
+        return false;
+    }
+}
