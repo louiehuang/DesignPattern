df8c6a1cc8728707f943c75912e03c1a57207ea7
louiehuang <louiehuang21@gmail.com>
2019-05-18 19:50:22 -0700

    dao pattern

---
+++ src/main/java/com/miscellaneouspattern/daopattern/StudentDao.java
@@ -0,0 +1,10 @@
+package com.miscellaneouspattern.daopattern;
+
+import java.util.List;
+
+public interface StudentDao {
+    List<Student> getAllStudents();
+    Student getStudent(int rollNo);
+    void updateStudent(Student student);
+    void deleteStudent(Student student);
+}
